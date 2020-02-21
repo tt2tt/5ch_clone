996f8af3a874f9e69d4d3ff8c2f7dab735d04e4e
”taro <groveleffect@gmail.com>
2020-02-21 10:06:41 +0900

    スレッド投稿機能の作成

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -2,8 +2,28 @@ class BoardsController < ApplicationController
   def index
     if params[:search]
       @boards = Board.where(category_id: params[:search][:category_id])
+      @board = Board.new
     else
       @boards = Board.all
     end
   end
+
+  def create
+    @board = current_user.boards.build(board_params)
+	  if current_user
+      if @board.save
+        redirect_to categories_path
+      else
+        redirect_to categories_path
+      end
+    else
+      redirect_to user_session_path
+    end
+  end
+
+  private
+
+	def board_params
+	  params.require(:board).permit(:title, :content, :category_id)
+	end
 end
